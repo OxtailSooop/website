be398a3f2989aa22723ac58dacfa502700ca0af5
OxtailSooop <really@chalkydogshit.co.uk>
2026-01-28 20:59:04 +0000

    Move blog development to new branch

--- docker-entrypoint-initdb.d/init-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "blog" <<-EOSQL
-CREATE TABLE IF NOT EXISTS posts (id SERIAL PRIMARY KEY, title TEXT, date TEXT, content TEXT, media TEXT []);
-CREATE TABLE IF NOT EXISTS media (id SERIAL PRIMARY KEY, media TEXT);
-EOSQL
